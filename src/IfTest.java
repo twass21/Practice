f8edb0519dfcd235ade117b99b5a477c4909cff5
twass21 <42975595+twass21@users.noreply.github.com>
2018-11-09 12:45:53 -0500

    Add files via upload

---
+++ src/IfTest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/*
+ * @author Tyler Wassel
+ */
+public class IfTest
+{
+/*
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter a test score: ");
+		//double score = Double.parseDouble(in.nextLine());
+		double score = in.nextDouble();
+		
+		if(score >=60)
+		{
+			System.out.println("You passed the test");
+		}
+		else
+		{
+		System.out.println("you failed the test");
+		}
+		*/
+	
+	}
+Scanner in = new Scanner (System.in);
+System.out.print("Please enter an age: ");
+int age = in.nextInt();
+if(age <=65)
+		
+}
+
+
+	
+	
